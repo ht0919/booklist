628ca1aa0f3b229ecf2b4a346fbe269fe0497691
Hiroyuki Tanaka <ht0919@gmail.com>
2016-11-13 13:32:39 +0900

    Add root routing

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   resources :books
   resources :publishers
   resources :authers
+  root 'books#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
